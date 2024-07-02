c718fe2b82bfa08ffd89f4d0e857329fdec135b2
chaitu1 <chaitu143@gmail.com>
2024-07-02 23:51:20 +0530

    docker file for mongo

--- roboshop-v1/schema.sh
+++ roboshop-v1/schema.sh
@@ -1,6 +1,8 @@
-[ -z "${GIT_HUB_URL}" ] && echo "GIT_HUB_URL -- env parameter is missing" && exit 1
+[ -z "${CODE_URL}" ] && echo "GIT_HUB_URL -- env parameter is missing" && exit 1
 [ -z "${SCHEMA_FILE}" ] && echo "SCHEMA_FILE -- env parameter is missing" && exit 1
 [ -z "${MONGO_DB_ADDRESS}" ] && echo "MONGO_DB_ADDRESS -- env parameter is missing" && exit 1
 
-git clone ${GIT_HUB_URL} /app
+mkdir -p /app
+curl -o /tmp/code.zip ${CODE_URL}
+uzip /tmp/code.zip -d /app
 mongo --host ${MONGO_DB_ADDRESS} < ${SCHEMA_FILE}
