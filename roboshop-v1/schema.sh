ea52d8e493107a05c186182f6e472e2f696edd3f
chaitu1 <chaitu143@gmail.com>
2024-07-04 23:13:45 +0530

    shipping & mysql is ready

--- roboshop-v1/schema.sh
+++ roboshop-v1/schema.sh
@@ -16,6 +16,8 @@ if [ "${component}" == "mysql" ]; then
 [ -z "${CODE_URL}" ] && echo "GIT_HUB_URL -- env parameter is missing" && exit 1
 [ -z "${SCHEMA_FILE}" ] && echo "SCHEMA_FILE -- env parameter is missing" && exit 1
 [ -z "${MYSQL_ADDRESS}" ] && echo "MYSQL_ADDRESS -- env parameter is missing" && exit 1
+[ -z "${MYSQL_USER}" ] && echo "MYSQL_USER -- env parameter is missing" && exit 1
+[ -z "${MYSQL_PASSWORD}" ] && echo "MYSQL_PASSWORD -- env parameter is missing" && exit 1
 mkdir -p /app
 curl -o /tmp/code.zip ${CODE_URL}
 unzip /tmp/code.zip -d /app
