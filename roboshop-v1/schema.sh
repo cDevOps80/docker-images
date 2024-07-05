61fc0754473a74a59da96293c0e1445866a24e36
chaitu1 <chaitu143@gmail.com>
2024-07-05 20:34:14 +0530

    ss

--- roboshop-v1/schema.sh
+++ roboshop-v1/schema.sh
@@ -23,9 +23,9 @@ curl -o /tmp/code.zip ${CODE_URL}
 unzip /tmp/code.zip -d /app
 sleep 30
 #for i in $(ls ${SCHEMA_FILE}) ; do  mysql -h ${MYSQL_ADDRESS}  -u${MYSQL_USER} -p${MYSQL_PASSWORD} < ${SCHEMA_FILE}/${i} ; done
-mysql -h ${MYSQL_ADDRESS} -u${MYSQL_USER} -p${MYSQL_PASSWORD} < /app/db/schema.sql
-mysql -h ${MYSQL_ADDRESS} -u${MYSQL_USER} -p${MYSQL_PASSWORD} < /app/db/app-user.sql
-mysql -h ${MYSQL_ADDRESS} -u${MYSQL_USER} -p${MYSQL_PASSWORD} < /app/db/master-data.sql
+mysql -h ${MYSQL_ADDRESS} -u${MYSQL_USER} -p${MYSQL_PASSWORD} < ${SCHEMA_FILE}/schema.sql
+mysql -h ${MYSQL_ADDRESS} -u${MYSQL_USER} -p${MYSQL_PASSWORD} < ${SCHEMA_FILE}/app-user.sql
+mysql -h ${MYSQL_ADDRESS} -u${MYSQL_USER} -p${MYSQL_PASSWORD} < ${SCHEMA_FILE}/master-data.sql
 
 fi
 
